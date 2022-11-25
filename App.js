6d42745faa949f790d2975124ba6da6cf1d6dd66
CharukeasT <118506571+CharukeasT@users.noreply.github.com>
2022-11-25 16:25:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import DynamicForm from './DynamicForm'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <DynamicForm/>
+      </div>
+    )
+  }
+}
+
+export default App
